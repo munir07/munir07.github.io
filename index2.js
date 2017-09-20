0aef748390f89cda68fe0a683ede85fe4784b387
munir07 <kadersmm@Muhammads-MacBook-Pro.local>
2017-09-20 19:00:56 +0800

    Imran's Profile

---
+++ index2.js
@@ -0,0 +1,8 @@
+// Start writing JS script here
+//alert("welcome to class")
+var student = "Muneer";
+var student_1 = "Sakeenah";
+var student_2 = "Sofiyyah";
+
+var welcome_msg = "Hi. " + student_1 + " and " + student_2 + " are best friends";
+console.log(welcome_msg)
