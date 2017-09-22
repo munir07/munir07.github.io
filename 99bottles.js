262ba42f1360737aaafd43715fdf3d8ca4da1077
munir07 <kadersmm@Muhammads-MacBook-Pro.local>
2017-09-22 12:09:50 +0800

    added imran folder

--- 99bottles.js
+++ 99bottles.js
@@ -1,29 +1,3 @@
-// 99 bottles of beer on the wall, 99 bottles of beer.
-// Take one down and pass it around, 98 bottles of beer on the wall.
- 
-// 98 bottles of beer on the wall, 98 bottles of beer.
-// Take one down and pass it around, 97 bottles of beer on the wall.
- 
-// 97 bottles of beer on the wall, 97 bottles of beer.
-// Take one down and pass it around, 96 bottles of beer on the wall.
- 
-// 96 bottles of beer on the wall, 96 bottles of beer.
-// Take one down and pass it around, 95 bottles of beer on the wall.
- 
-// 95 bottles of beer on the wall, 95 bottles of beer.
-// Take one down and pass it around, 94 bottles of beer on the wall.
- 
-// ....
- 
-// 3 bottles of beer on the wall, 3 bottles of beer.
-// Take one down and pass it around, 2 bottles of beer on the wall.
- 
-// 2 bottles of beer on the wall, 2 bottles of beer.
-// Take one down and pass it around, 1 bottle of beer on the wall.
- 
-// 1 bottle of beer on the wall, 1 bottle of beer.
-// Go to the store and buy some more, 99 bottles of beer on the wall.
-
 var numBottles = 99;
 var line1;
 var line2;
--- myprofile.js
+++ myprofile.js
@@ -1,6 +1,55 @@
-function processInput () {  
-    var name = document.getElementById("name-id").value
-    var email = document.getElementById("email-id").value
-    var str = "Hello " + name + "    (" + email + ")"
-    alert(str)
-}
+// function processInput () {  
+//     var name = document.getElementById("name-id").value
+//     var email = document.getElementById("email-id").value
+//     var str = "Hello " + name + "    (" + email + ")"
+//     alert(str)
+// }
+
+$(function () {
+    
+        // on submitting the form
+        $('form').submit(function (event) {
+            // prevent the default action of reloading the page
+            event.preventDefault();
+    
+            var sendData = {};
+            $(event.target.nodeName + ' :input').each(function(){
+                sendData[this.name] = $(this).val();
+            });
+    
+            var posting = $.ajax({
+                type: 'POST',
+                url: $(event.target.nodeName).prop('action'),
+                data: sendData
+            });
+    
+            posting.done(function (response) {
+                console.log(response);
+                $('#alert-id').prop('hidden', false);
+                $('form :input').each(function(){
+                    $(this).val('');
+                })
+            });
+            posting.fail(function (response) {
+                console.log(response);
+            });
+        });
+    
+    // RESPONSE ALERT WINDOW-------------------------------------------------------------------------------
+    /* include the following HTML to use:
+    <div class="form-group">
+        <button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
+        <div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
+            <button type="button" class="close" id="close-id"><span>&times;</span></button>
+            Thank you! We'll get in touch.
+        </div>
+    </div>
+    */
+    
+        // on clicking the X button
+        $('#close-id').click(function(){
+            // hide the alert panel by adding the hidden property
+            $('#alert-id').prop('hidden', true);
+        });
+    
+    });
